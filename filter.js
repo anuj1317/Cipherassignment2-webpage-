d1dcdff2a657d3a67047be0dda207875286a6273
Anuj Singh <anujsingh1317@gmail.com>
2020-08-19 23:25:32 +0530

    first commit

---
+++ filter.js
@@ -0,0 +1,23 @@
+function filter(){
+    
+var filterValue, input, ul,li,a,i;
+ input = document.getElementById("search");
+ filterValue = input.value.toUpperCase();
+ul = document.getElementById("Menu");
+ li = ul.getElementsByTagName("li");
+ 
+ 
+    
+    for (i = 0 ; i < li.length ; i++){
+        a = li[i].getElementsByTagName("p")[0];
+        if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+            li[i].style.display = "";
+            
+        }else{
+            li[i].style.display = "none";
+        }
+    }
+   
+}
+
+
